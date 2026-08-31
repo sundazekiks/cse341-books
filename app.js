484e0b43106328ee3e86599946013ddbf8012a3f
sundazekiks <laqkiks@gmail.com>
2026-08-31 18:04:32 -0400

    connected app to mongodb

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ export const app = express()
 app.use(express.json())
 
 app.get("/", (req, res) => {
+
     return res.status(200).send("Server is healthy")
 })
 
---
+++ db/mongo.js
@@ -0,0 +1,22 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const URI = process.env["MONGODB_URI"];
+const DBNAME = process.env["MONGODB_NAME"];
+const client = new MongoClient(URI,
+    {
+        serverApi: ServerApiVersion.v1
+    }
+)
+
+export let db;
+
+export async function connect() {
+    try {
+        await client.connect();
+        await client.db(DBNAME).command({ ping: 1 });
+        console.log("Database connected successfuly")
+        db = client.db(DBNAME);
+    } catch (err) {
+        console.err("database failed to connect" + err)
+    }
+}
