a2859e24224a230273aa0304c3fe52845784ce67
sundazekiks <laqkiks@gmail.com>
2026-09-07 13:45:44 -0400

    added: swagger-docs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import books from './route/book.route.js'
 export const app = express()
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
+
 
 // JSON request body middleware
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // routes
 app.use("/books", books)
--- route/book.route.js
+++ route/book.route.js
@@ -3,6 +3,42 @@ import { GetBook, GetBooks } from "../controller/book.controller.js";
 
 const router = Router();
 
+
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Books returned successfully
+ *       500:
+ *         description: Unable to retrieve books
+ */
 router.get("/", GetBooks)
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get one book by id
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The custom book id, such as b1
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book returned successfully
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Unable to retrieve book
+ */
 router.get("/:id", GetBook)
 export default router;
